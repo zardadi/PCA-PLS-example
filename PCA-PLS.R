949ee13d136714bd5058787786f6e0ba159be15c
zardadi <zardadi@gmail.com>
2019-02-05 16:38:17 -0800

    Updated the R code

--- PCA-PLS.R
+++ PCA-PLS.R
@@ -34,60 +34,25 @@ library(pls)
 nuplsmod <- plsr(calories~., data=nutrition[,-c(1,28)], scale=TRUE, method="oscorespls")
 summary(nuplsmod)
 
-# Now it appears that 2 components are sufficient (cumlatively explained just over 89% of variation in 'calories').
+# Now it appears that 2 components are sufficient (cumlatively explained just over 89% of variation in 'calories'.
 # Compared to PCReg - 8 components explained only 79% of the variation in calories.
 # What if we take just to components for PCReg...
 ## ------------------------------------------------------------------------
 pcregmod2 <- lm(nutrition$calories ~ scr[,1:2])
 summary(pcregmod2)
 
-# We two components Adjusted R-squared is 0.23. Hence why pls is generally a superior approach in a supervised context.
+# We two PCS components Adjusted R-squared is 0.23. Hence why PLS is generally a superior approach in a supervised context.
 
 summary(lm(nutrition$calories ~ nuplsmod$scores[,1:2]))
 # If we use first two components of PLS, then we see Adjusted R-squared is 89% 
 ## ------------------------------------------------------------------------
+
 # A cautionary tale...
 dwmod <- lm(calories~total_fat+carbohydrates, data=nutrition)
 summary(dwmod)
 
-# If we use just plin data set and without any knowledge of PCA, we might get better results
+# If we just use plain data set and without any knowledge of PCA, we might get better results!
 # Just slightly more of the variation (90.8% vs 89.1%) in calories can be explained using an equivalent dimensionlaity from
 # the original varibales.
 
-# Conclusion: in machine learning, there is often no substitude for domain-knowledge.
-
-## ------------------------------------------------------------------------
-#PCReg example
-# # simulation we walked through in class.
-set.seed(311351)
-x1 <- runif(40, 0, 10)
-x2 <- 2*x1 + rnorm(40)
-y <- x2-2*x1 + rnorm(40, sd=.1)
-linmod <- lm(y~x1+x2)
-summary(linmod)
-#wow, really strong!
-pcs <- prcomp(cbind(x1, x2))
-summary(pcs)
-#by any standard, we would remove PC2, so...
-linpc <- lm(y~pcs$x[,1])
-summary(linpc)
-#but guess what...
-linpc2 <- lm(y~pcs$x[,2])
-summary(linpc2)
-#but jeff, you didn't standardize
-pcss <- prcomp(cbind(x1, x2), scale.=TRUE)
-summary(pcss)
-#all the same story
-linpcs <- lm(y~pcss$x[,1])
-summary(linpcs)
-linpcs2 <- lm(y~pcss$x[,2])
-summary(linpcs2)
-pairs(cbind(y, pcss$x))
-
-## ------------------------------------------------------------------------
-plsmod <- plsr(y~x1+x2, method="oscorespls")
-summary(plsmod)
-#but i didn't standardize again!
-plsmods <- plsr(y~x1+x2, method="oscorespls", scale=TRUE)
-summary(plsmods)
-
+# Conclusion: in machine learning, there is often no substitute for domain-knowledge.
